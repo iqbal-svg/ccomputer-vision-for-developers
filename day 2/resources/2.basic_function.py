f792a4d8f4a40cd89de0e7cf2b30721aa7ef67c8
Iqbal Shaikh <iqbalshaikhsap@gmail.com>
2025-09-16 12:28:20 +0530

    cv2 fn

--- day 2/resources/2.basic_function.py
+++ day 2/resources/2.basic_function.py
@@ -3,10 +3,42 @@ import numpy as np
 
 ## 1. Convert to gray scale
 
+# img = cv2.imread('lena.png')
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# print("Color_img Shape: ", img.shape)
+# print("img_gray Shape: ", img_gray.shape)
+# cv2.imshow("Color_img", img)
+# cv2.imshow("Gray_img", img_gray)
+# cv2.waitKey(0)
+
+#Convert to blurr
+
+# img = cv2.imread('lena.png')
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img_blur = cv2.GaussianBlur(img_gray, (7,7), 0)
+# print("Color_img Shape: ", img.shape)
+# print("img_gray Shape: ", img_gray.shape)
+# print("img_blur Shape: ", img_blur.shape)
+# cv2.imshow("Color_img", img)
+# cv2.imshow("Gray_img", img_gray)
+# cv2.imshow("img_blur", img_blur)
+# cv2.waitKey(0)
+
+# 3. Convert to cannyImg
+
 img = cv2.imread('lena.png')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_blur = cv2.GaussianBlur(img_gray, (7,7), 0)
+img_canny = cv2.Canny(img_blur, 100,100)
 print("Color_img Shape: ", img.shape)
 print("img_gray Shape: ", img_gray.shape)
+print("img_blur Shape: ", img_blur.shape)
+print("img_canny Shape: ", img_canny.shape)
 cv2.imshow("Color_img", img)
 cv2.imshow("Gray_img", img_gray)
+cv2.imshow("img_blur", img_blur)
+cv2.imshow("img_canny", img_canny)
 cv2.waitKey(0)
+
+
+
